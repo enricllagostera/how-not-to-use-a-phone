722ff809992384592f63011e1a77fb0fd819e2a1
Enric Llagostera <enricllagostera@gmail.com>
2019-01-31 09:35:43 -0500

    work on js tests and new unity plugins

---
+++ sensors-plugin-test/Assets/Coral.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Shellphone
+{
+    public class Coral : MonoBehaviour
+    {
+        public Sea sea;
+        public Coral prefabCoral;
+        public CoralInfo info;
+        public Coral parent;
+        public bool isRoot;
+        public bool isLeaf;
+        public bool hasStopped;
+        public float nextRollTime;
+        public float baseScale;
+        public float chanceToBranch;
+        public float chanceToStop;
+        private SpriteRenderer _sprite;
+
+        void Start()
+        {
+            _sprite = GetComponent<SpriteRenderer>();
+            _sprite.color = info.initialColors.Evaluate(sea.health);
+            if (isRoot)
+            {
+                baseScale = Random.Range(info.baseStartScale * (1f - info.baseScaleRange), info.baseStartScale * (1f + info.baseScaleRange));
+            }
+            else
+            {
+                baseScale = 1f * info.branchScaleDecay;
+            }
+            transform.localScale = Vector3.one * baseScale;
+            chanceToBranch = 0f;
+            chanceToStop = 0f;
+            hasStopped = false;
+            nextRollTime = CalculateTimeToNextRoll();
+        }
+
+        private float CalculateTimeToNextRoll()
+        {
+            return Time.realtimeSinceStartup + Random.Range(info.branchCooldown * (1f - info.branchCooldownRange), info.branchCooldown * (1f + info.branchCooldownRange));
+        }
+
+        void Update()
+        {
+            if (hasStopped)
+            {
+                return;
+            }
+
+            if (nextRollTime <= Time.realtimeSinceStartup)
+            {
+                nextRollTime = CalculateTimeToNextRoll();
+                UpdateChanceToStop();
+                UpdateChanceToBranch();
+                if (WillBranchOut())
+                {
+                    Debug.Log(name + " BRANCH OUT");
+                    sea.CreateCoral(this);
+                }
+                else if (WillStopBranching())
+                {
+                    Debug.Log(name + " STOPPED BRANCHING");
+                    hasStopped = true;
+                }
+            }
+        }
+
+        private void UpdateChanceToBranch()
+        {
+            chanceToBranch = info.baseChanceToBranch;
+        }
+
+        private void UpdateChanceToStop()
+        {
+            chanceToStop = info.baseChanceToStop;
+        }
+
+        private bool WillStopBranching()
+        {
+            return (chanceToStop <= Random.value);
+        }
+
+        private bool WillBranchOut()
+        {
+            return (chanceToBranch <= Random.value);
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.white;
+            Gizmos.DrawWireSphere(transform.position, baseScale);
+        }
+    }
+
+}
---
+++ sensors-plugin-test/Assets/DebugTextGUI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityAndroidSensors.Scripts.Utils.SmartVars;
+using TMPro;
+
+namespace SensorTest
+{
+    public class DebugTextGUI : MonoBehaviour
+    {
+        private TextMeshProUGUI msgGUI;
+        public FloatVar varValue;
+
+        void Start()
+        {
+            msgGUI = GetComponent<TextMeshProUGUI>();
+        }
+
+        void Update()
+        {
+            msgGUI.text = varValue.value.ToString("000.000");
+        }
+    }
+}
+
+
